4906d09e98aff4b9ed3a748e54b9a1d1487ba234
BUIXUANHUYNH1995 <huynhxuanbui@gamil.com>
2019-09-26 13:37:25 +0700

    how to use operator

---
+++ operator.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width")
+inputHeight = prompt("Enter the height")
+let width = parseInt(inputWidth);
+let hight = parseInt(inputHeight);
+let area = width*hight;
+document.write("The area is: " + area);
